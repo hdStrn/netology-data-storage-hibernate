f8bd147f83bc6f05f3b6d73f8f6c39876c6fcb40
hdStrn <nomovemc@gmail.com>
2023-03-17 11:11:43 +0300

    HQL request optimized

--- src/main/java/ru/netology/netologydatastoragehibernate/dao/PersonDaoImpl.java
+++ src/main/java/ru/netology/netologydatastoragehibernate/dao/PersonDaoImpl.java
@@ -2,14 +2,11 @@ package ru.netology.netologydatastoragehibernate.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.netology.netologydatastoragehibernate.entity.Person;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,13 +17,9 @@ public class PersonDaoImpl implements PersonDao {
 
     @Override
     public List<Person> getPersonsByCity(String city) {
-//        Query selectByCity = entityManager.createQuery(
-//                "select p from Person p where p.cityOfLiving = :city", Person.class);
-//        selectByCity.setParameter("city", city);
-//        return selectByCity.getResultList();
-
-        return entityManager.createQuery("select p from Person p", Person.class).getResultStream()
-                .filter(p -> Objects.equals(p.getCityOfLiving().toLowerCase(), city.toLowerCase()))
-                .collect(Collectors.toList());
+        return entityManager
+                .createQuery("select p from Person p where p.cityOfLiving = :city", Person.class)
+                .setParameter("city", city)
+                .getResultList();
     }
 }
