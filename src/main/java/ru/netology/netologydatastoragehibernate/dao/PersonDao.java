6e20c891f1af6bd74030cbeb19d55de34c302f6b
hdStrn <nomovemc@gmail.com>
2023-03-19 14:30:16 +0300

    Repo methods annotated by query

--- src/main/java/ru/netology/netologydatastoragehibernate/dao/PersonDao.java
+++ src/main/java/ru/netology/netologydatastoragehibernate/dao/PersonDao.java
@@ -1,6 +1,8 @@
 package ru.netology.netologydatastoragehibernate.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.netologydatastoragehibernate.entity.Person;
 import ru.netology.netologydatastoragehibernate.entity.PersonalData;
@@ -11,9 +13,12 @@ import java.util.Optional;
 @Repository
 public interface PersonDao extends JpaRepository<Person, PersonalData> {
 
+    @Query(value = "SELECT * FROM person p WHERE p.city_of_living = :city", nativeQuery = true)
     List<Person> findAllByCityOfLiving(String city);
 
+    @Query(value = "SELECT * FROM person p WHERE p.age < :age ORDER BY p.age", nativeQuery = true)
     List<Person> findAllByPersonalData_AgeLessThanOrderByPersonalData_Age(Integer age);
 
+    @Query(value = "SELECT * FROM person p WHERE p.name = :name AND p.surname = :surname", nativeQuery = true)
     Optional<Person> findAllByPersonalData_NameAndPersonalData_Surname(String name, String surname);
 }
